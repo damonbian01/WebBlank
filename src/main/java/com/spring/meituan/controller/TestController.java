978237338f75302a7b703c813ba1c5300500d45a
biantao <biantao@meituan.com>
2016-06-30 11:35:54 +0800

    add ModelAndView

--- src/main/java/com/spring/meituan/controller/TestController.java
+++ src/main/java/com/spring/meituan/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -31,5 +33,19 @@ public class TestController {
 
         request.setAttribute("ids", ids);
         return "test";
+    }
+
+    @RequestMapping ( value = "/testModelView", method = RequestMethod.GET)
+    public ModelAndView testModelView() {
+        ModelAndView mv = new ModelAndView("test");
+        String sql = "select id from tb_user";
+        List<Integer> ids = jdbcTemplate.query(sql, new RowMapper<Integer>() {
+            public Integer mapRow(ResultSet resultSet, int i) throws SQLException {
+                return resultSet.getInt("id");
+            }
+        });
+
+        mv.addObject("ids", ids);
+        return mv;
     }
 }
