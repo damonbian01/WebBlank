58efdc2fad1c718baf55b7313447ff033466bda7
biantao <biantao@meituan.com>
2016-07-01 08:54:25 +0800

    alter xml

--- src/main/java/com/spring/meituan/controller/TestController.java
+++ src/main/java/com/spring/meituan/controller/TestController.java
@@ -1,24 +1,26 @@
 package com.spring.meituan.controller;
 
 import com.spring.meituan.vo.TestUservo;
-import com.spring.meituan.vo.TestUservo;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Serializable;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by biantao on 16/6/29.
@@ -27,6 +29,8 @@ import java.util.List;
 public class TestController {
     @Resource
     private JdbcTemplate jdbcTemplate;
+    @Resource
+    private FreeMarkerConfigurer freemarkerConfig;
 
     @RequestMapping( value = "/test")
     public String test(HttpServletRequest request) {
@@ -73,6 +77,21 @@ public class TestController {
         String json = "{\"data\":\"nihao\"}";
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().println(json);
+    }
+
+    @RequestMapping ( value = "/testFtl")
+    public void testFtl(HttpServletResponse response) throws IOException, TemplateException {
+        Template template = freemarkerConfig.getConfiguration().getTemplate("test.ftl");
+        String sql = "select id from tb_user";
+        List<Integer> ids = jdbcTemplate.query(sql, new RowMapper<Integer>() {
+            public Integer mapRow(ResultSet resultSet, int i) throws SQLException {
+                return resultSet.getInt("id");
+            }
+        });
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("ids", ids);
+        String str = FreeMarkerTemplateUtils.processTemplateIntoString(template, (Object) map);
+        response.getWriter().println(str);
     }
 
 }
